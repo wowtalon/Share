e95010f93eb2f10a3981aa18a218f02a301d2f2b
zhuangshq <1120692265@qq.com>
2016-08-31 22:32:40 +0800

    first

---
+++ gen/cn/zhuangshq/share/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package cn.zhuangshq.share;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/cn/zhuangshq/share/MainActivity.java
@@ -0,0 +1,58 @@
+package cn.zhuangshq.share;
+
+import android.app.ActionBar.LayoutParams;
+import android.app.Activity;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.activity_main);
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {    
+		    getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);    
+		    getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);    
+		}  
+//		final LinearLayout container = (LinearLayout) findViewById(R.id.container);
+//		LinearLayout main = (LinearLayout) findViewById(R.id.main);
+//		
+//		LinearLayout.LayoutParams layoutParams = (android.widget.LinearLayout.LayoutParams) main.getLayoutParams();
+//		layoutParams.height = container.getHeight();
+//		//layoutParams.topMargin = 0;
+//		layoutParams.width = container.getWidth();
+//		
+//		
+//		main.setLayoutParams(layoutParams);
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		//getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
